b50bd76b9fed113cfb4c7396df4d19ef1b4bed27
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-07-04 15:54:43 +0530

    Initial Version

--- src/Trails.Portal/gulpfile.js
+++ src/Trails.Portal/gulpfile.js
@@ -3,7 +3,7 @@ This file in the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 "use strict";
-
+ 
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
