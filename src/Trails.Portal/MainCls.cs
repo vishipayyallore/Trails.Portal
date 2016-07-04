52b4838528268618ff4563848466d442a752bf30
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-07-04 15:54:10 +0530

    Add project files.

---
+++ src/Trails.Portal/MainCls.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Trails.Portal
+{
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
